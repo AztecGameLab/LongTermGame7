1f379e8d1aaa0e40d87d22f4b563e216241cc2e2
Daniel Walls <walls.dk@gmail.com>
2022-12-06 21:36:37 -0800

    updated readme

--- Assets/Scripts/Entrypoint.cs
+++ Assets/Scripts/Entrypoint.cs
@@ -5,7 +5,8 @@ using Logger = Core.Logger;
 
 /// <summary>
 /// This class should be the first one that is loaded in the game.
-/// It controls the startup for the game systems.
+/// It should persist for the entire application lifetime, only being destroyed when the application quits.
+/// It controls the startup, updating, and shutdown of the game sub-systems.
 /// </summary>
 public class Entrypoint : MonoBehaviour
 {
@@ -15,8 +16,44 @@ public class Entrypoint : MonoBehaviour
     private VfxSystem _vfxSystem;
     private UISystem _uiSystem;
 
+    #region Initialization Validation
+
+        private static bool _initialized;
+    
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        private static void Init()
+        {
+            _initialized = false;
+        }
+        
+        private void Awake()
+        {
+            _initialized = true;
+        }
+        
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+        private static void CheckForAwake()
+        {
+            if (_initialized == false)
+            {
+                Debug.LogError("Entrypoint must be initialized before anything else!");
+                Application.Quit();
+
+                #if UNITY_EDITOR
+                
+                    UnityEditor.EditorApplication.isPlaying = false;                
+                
+                #endif
+            }
+        }
+
+    #endregion
+    
     private void Start()
     {
+        // Basic implementation of scene persistence. Could move to a dedicated persistent scene, but that is hard.
+        DontDestroyOnLoad(this);
+        
         // Demo of how we could implement cross-cutting concerns.
         // Ensures global access, polymorphism, and control over construction order + dependencies.
         Services.Console = new Console();
@@ -31,6 +68,9 @@ public class Entrypoint : MonoBehaviour
         _gameplaySystem = new GameplaySystem(settings.Gameplay);
         _uiSystem = new UISystem(settings.Ui);
         _levelSystem = new LevelSystem(settings.Level);
+        
+        // A demo to showcase how all the sub-systems might come together to manage the game.
+        _levelSystem.RunDemo();
     }
 
     private void OnApplicationQuit()
--- Assets/Scripts/Level/LevelSettings.cs
+++ Assets/Scripts/Level/LevelSettings.cs
@@ -4,5 +4,6 @@ using UnityEngine;
 [CreateAssetMenu(menuName = ApplicationConstants.AssetMenuName + "/"  + LevelConstants.AssetMenuName + "/Settings")]
 public class LevelSettings : ScriptableObject
 {
-    
+    // todo: annotation for easy scene selection
+    public string demoSceneName;
 }
