b6ea4af2e98f757c8d689eb289832b09c57ece6e
Daniel Walls <walls.dk@gmail.com>
2023-01-06 17:55:03 -0800

    physics abstractions

---
+++ Assets/Scripts/Application/Gameplay/InputLocal.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Application.Gameplay
+{
+    public class InputLocal : MonoBehaviour
+    {
+        public Vector2 InputDirection => 
+            new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        // and so on, with different input data.
+    }
+}
