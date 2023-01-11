e5bd027b7e624c7526909869adec9e9f54f39595
Daniel Walls <walls.dk@gmail.com>
2023-01-11 01:12:18 -0800

    got animations working, could probably use more documentation

---
+++ Assets/Scripts/Application/Vfx/Animation/FlipbookAnimationData.cs
@@ -0,0 +1,16 @@
+﻿using Application;
+using Application.Vfx;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = ApplicationConstants.AssetMenuName + "/" + VfxConstants.AssetMenuName + "/Flipbook Animation Data")]
+public class FlipbookAnimationData : ScriptableObject
+{
+    [SerializeField]
+    [Tooltip("The material that should be applied for this animation.")]
+    private Material animationMaterial;
+
+    /// <summary>
+    /// The material that should be applied for this animation.
+    /// </summary>
+    public Material Material => animationMaterial;
+}
