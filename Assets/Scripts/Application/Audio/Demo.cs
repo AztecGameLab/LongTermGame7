1863454f66588b99ee293c07774b76ec27d69c38
Daniel Walls <walls.dk@gmail.com>
2023-01-11 14:08:38 -0800

    audio demo

---
+++ Assets/Scripts/Application/Audio/Demo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using FMOD.Studio;
+using FMODUnity;
+using UnityEngine;
+using STOP_MODE = FMOD.Studio.STOP_MODE;
+
+namespace Application.Audio
+{
+    public class Demo : MonoBehaviour
+    {
+        private void Start()
+        {
+            
+        }
+    }
+}
