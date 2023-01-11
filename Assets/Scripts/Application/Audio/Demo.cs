6135704bb5f07d4facf7ed3bceabdd6b62f8a6a1
Daniel Walls <walls.dk@gmail.com>
2023-01-11 14:33:54 -0800

    demo showcase of fmod API

--- Assets/Scripts/Application/Audio/Demo.cs
+++ Assets/Scripts/Application/Audio/Demo.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using FMOD;
 using FMOD.Studio;
 using FMODUnity;
 using UnityEngine;
@@ -8,9 +9,27 @@ namespace Application.Audio
 {
     public class Demo : MonoBehaviour
     {
+        public EventReference reference;
+        public float value;
+
+        private EventInstance _instance;
+        
         private void Start()
         {
+            // RuntimeManager.PlayOneShot(reference);
             
+            _instance = RuntimeManager.CreateInstance(reference);
+            _instance.start();
+        }
+
+        private void Update()
+        {
+            _instance.setParameterByName("Intensity", value);
+        }
+
+        private void OnDestroy()
+        {
+            _instance.stop(STOP_MODE.ALLOWFADEOUT);
         }
     }
 }
