59130f10ac367922bb084f1bff9da3034b98f7dd
AlexLenh7 <113726489+AlexLenh7@users.noreply.github.com>
2023-01-20 14:42:13 -0800

    Camera Dither Fade (#101)
    * added camera dither effect
    
    needs materials for testing
    
    * dither fade scene
    
    * added some material textures to test
    
    * shadow casting and directory restructure
    
    Co-authored-by: Daniel Walls <walls.dk@gmail.com>

---
+++ Assets/Scripts/Application/Vfx/ShadowCaster.cs
@@ -0,0 +1,28 @@
+﻿namespace Application.Vfx
+{
+    using UnityEngine;
+    using UnityEngine.Rendering;
+
+    /// <summary>
+    /// Creates a new, invisible shadow-only mesh.
+    /// </summary>
+    public class ShadowCaster : MonoBehaviour
+    {
+        [SerializeField]
+        private Material material;
+
+        private void Awake()
+        {
+            var originalFilter = GetComponent<MeshFilter>();
+
+            var shadowCasterObject = new GameObject($"{gameObject.name}'s Shadow Caster");
+            shadowCasterObject.transform.SetParent(transform, false);
+
+            var newRenderer = shadowCasterObject.AddComponent<MeshRenderer>();
+            var newFilter = shadowCasterObject.AddComponent<MeshFilter>();
+            newFilter.sharedMesh = originalFilter.sharedMesh;
+            newRenderer.material = material;
+            newRenderer.shadowCastingMode = ShadowCastingMode.ShadowsOnly;
+        }
+    }
+}
