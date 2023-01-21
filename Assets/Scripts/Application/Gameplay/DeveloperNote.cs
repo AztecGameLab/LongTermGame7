a4b1e65061784d2e8a2b02f7fd79d1abb205d132
Daniel Walls <walls.dk@gmail.com>
2023-01-21 00:23:41 -0800

    developer notes

---
+++ Assets/Scripts/Application/Gameplay/DeveloperNote.cs
@@ -0,0 +1,45 @@
+﻿namespace Application.Gameplay
+{
+    using TMPro;
+    using UnityEngine;
+
+    /// <summary>
+    /// A utility for writing 3D text notes into the scene.
+    /// </summary>
+    public class DeveloperNote : MonoBehaviour
+    {
+        [SerializeField]
+        private Vector2 size = new Vector2(3.5f, 1.5f);
+
+        [SerializeField]
+        [TextArea(minLines: 10, maxLines: 20)]
+        private string text = "Developer Note";
+
+        [SerializeField]
+        private HorizontalAlignmentOptions horizontalAlignment = HorizontalAlignmentOptions.Left;
+
+        [SerializeField]
+        private VerticalAlignmentOptions verticalAlignment = VerticalAlignmentOptions.Middle;
+
+        private void Awake()
+        {
+            ApplyText();
+        }
+
+        private void OnValidate()
+        {
+            ApplyText();
+        }
+
+        private void ApplyText()
+        {
+            var tmpText = GetComponentInChildren<TMP_Text>();
+            tmpText.text = text;
+            tmpText.horizontalAlignment = horizontalAlignment;
+            tmpText.verticalAlignment = verticalAlignment;
+
+            var rectTransform = GetComponentInChildren<Canvas>().GetComponent<RectTransform>();
+            rectTransform.sizeDelta = size;
+        }
+    }
+}
