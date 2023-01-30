db28d10667cd24ee9449c90e634e8a6cb569b78b
Daniel Walls <walls.dk@gmail.com>
2023-01-30 11:54:28 -0800

    fog settings, new testing scene

---
+++ Assets/Scripts/Application/Gameplay/LevelLoadingIdea.cs
@@ -0,0 +1,35 @@
+﻿// using Application.Core;
+// using UnityEngine;
+//
+// public class LevelEntrance : MonoBehaviour
+// {
+//     // has an id
+// }
+//
+// public class LevelExit : MonoBehaviour
+// {
+//     // When a player steps inside a trigger with this component, fire a levelChangeEvent.
+//     public void FireEvent()
+//     {
+//         Services.EventBus.Invoke(new LevelChangeEvent{sceneName = ...., entranceId = ....});
+//     }
+// }
+//
+// public class LevelChangeEvent
+// {
+//     public string sceneName;
+//     public string entranceId;
+// }
+//
+// public class LevelLoader
+// {
+//     public LevelLoader()
+//     {
+//         Services.EventBus.AddListener(HandleEventChange, "Level Loader");
+//     }
+//
+//     private void HandleLevelChange()
+//     {
+//         
+//     }
+// }
