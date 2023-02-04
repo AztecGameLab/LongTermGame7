ca3f778ca9c764f557916ce2113bc639c0bc41e1
Mitchell Shapiro <mzshap@gmail.com>
2023-02-03 17:47:37 -0800

    Added initial region tracking

---
+++ Assets/Scripts/Application/Core/OnSceneLoad.cs
@@ -0,0 +1,20 @@
+namespace Application.Core
+{
+    using Application.Core.ScriptableObjects;
+    using UnityEngine;
+
+    /// <summary>
+    /// Script that should run when a scene loads to set GameData values
+    /// </summary>
+    public class OnSceneLoad : MonoBehaviour
+    {
+        [SerializeField]
+        private GameData.Region _thisRegion = GameData.Region.Undefined;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            GameData.CurrentRegion = _thisRegion;
+        }
+    }
+}
---
+++ Assets/Scripts/Application/Core/ScriptableObjects/GameData.cs
@@ -0,0 +1,39 @@
+namespace Application.Core.ScriptableObjects
+{
+    using UnityEngine;
+
+    /// <summary>
+    /// Data to be made available
+    /// </summary>
+    public class GameData : ScriptableObject
+    {
+        // the current region
+        private static Region _currentRegion = Region.Undefined;
+
+        public enum Region
+        {
+            Undefined,
+            Forest,
+            Plains,
+            Mountains
+        }
+
+        /// <summary>
+        /// Gets or sets the current region
+        /// </summary>
+        /// <value>The current region.</value>
+        public static Region CurrentRegion
+        {
+            get
+            {
+                return _currentRegion;
+            }
+
+            set
+            {
+                Debug.Log("Updating region to: " + value);
+                _currentRegion = value;
+            }
+        }
+    }
+}
