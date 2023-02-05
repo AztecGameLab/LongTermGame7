f4d4fd222c9e3a38c9fc8a829e1a404ca6abcd71
jwellman13 <46513002+jwellman13@users.noreply.github.com>
2023-02-05 09:50:49 -0800

    State machine framework
    Framework for FSM implementation and code skeletons for previously discussed combat states.

---
+++ Assets/Scripts/Application/State Machine/States/PlayerActivityState.cs
@@ -0,0 +1,20 @@
+namespace Application.StateMachine
+{
+    public class PlayerActivityState : IState
+    {
+        public void OnEnter()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnExit()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnTick()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
