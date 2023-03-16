2fbf99089ed30848cf2cd79d38c40b5d853d1c02
Daniel Walls <walls.dk@gmail.com>
2023-03-16 16:47:31 -0700

    slight overhaul to registration of yarn commands for ease of use, interactable intergration

---
+++ Assets/Scripts/Application/Gameplay/IInteractable.cs
@@ -0,0 +1,16 @@
+﻿namespace Application.Gameplay
+{
+    using UnityEngine;
+
+    /// <summary>
+    /// Represents an object that can be interacted with.
+    /// </summary>
+    public interface IInteractable
+    {
+        /// <summary>
+        /// Run interaction logic for this entity.
+        /// </summary>
+        /// <param name="source">The object that is interacting with this object.</param>
+        void Interact(GameObject source);
+    }
+}
