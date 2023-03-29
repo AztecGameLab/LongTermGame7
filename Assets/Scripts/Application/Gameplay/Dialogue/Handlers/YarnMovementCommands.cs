c56dbdb62c715947af426845ab43c2b82b70927c
Daniel Walls <walls.dk@gmail.com>
2023-03-29 16:49:54 -0700

    got movement commands working

--- Assets/Scripts/Application/Gameplay/Dialogue/Handlers/YarnMovementCommands.cs
+++ Assets/Scripts/Application/Gameplay/Dialogue/Handlers/YarnMovementCommands.cs
@@ -1,17 +1,37 @@
-﻿using Yarn.Unity;
-
-namespace Application.Gameplay.Dialogue.Handlers
+﻿namespace Application.Gameplay.Dialogue.Handlers
 {
+    using System.Collections;
+    using Core.Utility;
+    using UniRx;
+    using UnityEngine;
+    using Yarn.Unity;
+
+    /// <summary>
+    /// Yarn commands for moving around characters in the world.
+    /// </summary>
     public class YarnMovementCommands : IYarnCommandHandler
     {
+        private DialogueRunner _runner;
+
+        /// <inheritdoc/>
         public void RegisterCommands(DialogueRunner runner)
         {
-            throw new System.NotImplementedException();
+            _runner = runner;
+            runner.AddCommandHandler<GameObject, GameObject>("move", MoveToCoroutine);
         }
 
+        /// <inheritdoc/>
         public void UnregisterCommands(DialogueRunner runner)
         {
-            throw new System.NotImplementedException();
+            runner.RemoveCommandHandler("move");
+        }
+
+        private Coroutine MoveToCoroutine(GameObject from, GameObject to) =>
+            _runner.StartCoroutine(MoveTo(from, to));
+
+        private static IEnumerator MoveTo(GameObject from, GameObject to)
+        {
+            yield return from.transform.PathFindTo(to.transform.position);
         }
     }
 }
