7c0de678c14b2bc0d8461f2488530522c197996e
Madison Bennett <114691834+madBennett@users.noreply.github.com>
2023-04-07 15:18:35 -0700

    Update RockPlatformStairsAction.cs

--- Assets/Scripts/Application/Gameplay/Combat/Actions/RockPlatformStairsAction.cs
+++ Assets/Scripts/Application/Gameplay/Combat/Actions/RockPlatformStairsAction.cs
@@ -4,7 +4,7 @@
     using System.Collections;
     using Core;
     using ImGuiNET;
-    // using UI.Indicators;
+    using UI.Indicators;
     using UnityEngine;
 
     [Serializable]
@@ -15,18 +15,33 @@
         private float maxRange = 5f; // max range the stairs can be placed away from the player
         private float stairsHeight = 1.5f;
         private float stairsWidth = 3f;
-        private AimSystem _aimSystem = new AimSystem();
         private Vector3 _targetPosition; // pos of stairs and new pos of player w/o new hight
 
-        private GameObject stairsPrefab; // object for stairs
-        private GameObject stairs; // public??????
+        private IPooledObject<RangeIndicator> _rangeIndicator;
+        private IPooledObject<ValidityIndicator> _validityIndicator;
+        private AimSystem _aimSystem = new AimSystem();
+        private ActionPointTracker _actionPointTracker;
+
+        public GameObject stairsPrefab; // object for stairs
+        public GameObject stairs;
 
         public override string Name => "Rock Stairs";
         public override string Description => "Summon stairs and move up them";
 
         public override void PrepEnter()
         {
+            // Setting up range indicator
+            _rangeIndicator = Services.IndicatorFactory.Borrow<RangeIndicator>();
+            _rangeIndicator.Instance.Range = maxRange;
+            DisposeOnExit(_rangeIndicator);
+
+            // Setting up validity indicator
+            _validityIndicator = Services.IndicatorFactory.Borrow<ValidityIndicator>();
+            _validityIndicator.Instance.IsValid = false;
+            DisposeOnExit(_validityIndicator);
+
             _aimSystem.Initialize(Camera.main);
+            _actionPointTracker = User.GetComponent<ActionPointTracker>();
         }
 
         public override void PrepTick()
@@ -37,9 +52,15 @@
             // Check if the stair placement will be in range/the space is not occupied
             float distance = Vector3.Distance(_targetPosition, User.transform.position);
 
-            if (distance <= maxRange && !Physics.CheckSphere(_targetPosition, stairsWidth))
+            if (distance <= maxRange && !Physics.CheckSphere(_targetPosition, stairsWidth)
+                && _actionPointTracker.CanAfford(actionPointCost))
             {
+                _validityIndicator.Instance.IsValid = true;
                 IsPrepFinished |= Input.GetKeyDown(KeyCode.Mouse0);
+            }
+            else
+            {
+                _validityIndicator.Instance.IsValid = false;
             }
         }
 
@@ -52,14 +73,15 @@
 
         protected override IEnumerator Execute()
         {
-            // Remove action points and exit
+            // Remove action points
             if (User.TryGetComponent(out ActionPointTracker tracker))
             {
                 tracker.TrySpend(actionPointCost);
             }
 
             // Summon stairs
-            stairs = Instantiate(stairsPrefab, _targetPosition);
+            stairs = UnityEngine.Object.Instantiate(stairsPrefab);
+            stairs.transform.position = _targetPosition;
 
             // Move up stairs
             _targetPosition = _targetPosition + new Vector3(0, stairsHeight, 0);
