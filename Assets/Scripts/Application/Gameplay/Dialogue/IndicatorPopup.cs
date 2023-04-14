7e19d906668acc256ece37ea88190bcb24277167
Alazerbeam <alazerbeam@outlook.com>
2023-04-14 15:38:09 -0700

    made quick changes to IndicatorPopup

--- Assets/Scripts/Application/Gameplay/Dialogue/IndicatorPopup.cs
+++ Assets/Scripts/Application/Gameplay/Dialogue/IndicatorPopup.cs
@@ -8,25 +8,22 @@ namespace Application.Gameplay.Dialogue
     {
         public GameObject indic;
         private Dictionary<Collider, GameObject> activeIndics = new Dictionary<Collider, GameObject>();
+        [SerializeField]
+        private float displacement;
 
         private void OnTriggerEnter(Collider other)
         {
             GameObject indicator = Instantiate(indic);
 
-            indicator.transform.position = new Vector3(other.transform.position.x, 
-                                            other.transform.position.y + 5f, other.transform.position.z);
+            indicator.transform.position = new Vector3( other.transform.position.x, 
+                                                        other.transform.position.y + displacement, 
+                                                        other.transform.position.z );
 
             activeIndics.Add(other, indicator);
         }
 
         private void OnTriggerExit(Collider other)
         {
-            //if (activeIndics.TryGetValue(other, out indicator))
-            //{
-            //    activeIndics.Remove(other);
-            //    Destroy(indicator);
-            //}
-
             GameObject indicator = activeIndics[other];
             Destroy(indicator);
             activeIndics.Remove(other);
