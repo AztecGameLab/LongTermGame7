e31de62ba01939550662314e4898c37702b9c9c6
Daniel Walls <walls.dk@gmail.com>
2023-04-14 12:58:03 -0700

    added enter and mouse click to the continue events

--- Assets/Scripts/Application/Gameplay/Dialogue/ContinueButton.cs
+++ Assets/Scripts/Application/Gameplay/Dialogue/ContinueButton.cs
@@ -1,22 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using Yarn.Unity;
 
-public class ContinueButton : MonoBehaviour
+public class ContinueButton : MonoBehaviour, IPointerClickHandler
 {
     public LineView lineView;
-    // Start is called before the first frame update
-    void Start()
+
+    private void Start()
     {
         lineView = GetComponent<LineView>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return))
+        {
             lineView.OnContinueClicked();
         }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        lineView.OnContinueClicked();
     }
 }
