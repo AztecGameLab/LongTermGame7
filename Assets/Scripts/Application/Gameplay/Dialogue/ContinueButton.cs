4b615cbfbb7ab2c7d9c15cf1dd0750a1e899aa1a
Isaelus <78238276+Isaelus@users.noreply.github.com>
2023-04-13 19:54:17 -0700

    Custom Line View
    You can now press the spacebar to continue dialog.

---
+++ Assets/Scripts/Application/Gameplay/Dialogue/ContinueButton.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class ContinueButton : MonoBehaviour
+{
+    public LineView lineView;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineView = GetComponent<LineView>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            lineView.OnContinueClicked();
+        }
+    }
+}
