6009735dcc3e2bc550e0506e149512a78459fea7
Daniel Walls <walls.dk@gmail.com>
2023-04-28 11:03:10 -0700

    added some more actions

---
+++ Assets/Scripts/Application/Gameplay/Combat/Actions/Dash.cs
@@ -0,0 +1,98 @@
+﻿namespace Application.Gameplay.Combat.Actions
+{
+    using System;
+    using System.Collections;
+    using Core;
+    using Core.Utility;
+    using ElRaccoone.Tweens;
+    using ElRaccoone.Tweens.Core;
+    using Newtonsoft.Json;
+    using UI.Indicators;
+    using UnityEngine;
+    using UnityEngine.AddressableAssets;
+
+    /// <summary>
+    /// Copy and paste this file to quickly get started with a new BattleAction.
+    /// </summary>
+    [Serializable]
+    [JsonObject(MemberSerialization.OptIn)]
+    public class Dash : BattleAction
+    {
+        [SerializeField]
+        [JsonProperty]
+        private string name = "Dash";
+
+        [SerializeField]
+        [JsonProperty]
+        private string description = "You dash forward, not stepping on anything in the stage.";
+
+        [SerializeField]
+        [JsonProperty]
+        private float distance = 3;
+
+        [SerializeField]
+        private int apCost = 6;
+
+        [SerializeField]
+        [JsonProperty]
+        private AssetReference dashEffect;
+
+        [SerializeField]
+        [JsonProperty]
+        private float moveDuration = 0.25f;
+
+        [SerializeField]
+        [JsonProperty]
+        private EaseType moveEasing = EaseType.Linear;
+
+        private IPooledObject<ArrowIndicator> _arrowIndicator;
+        private AimSystem _aimSystem = new AimSystem();
+        private Vector3 _targetPosition;
+
+        /// <inheritdoc/>
+        public override string Name => name;
+
+        /// <inheritdoc/>
+        public override string Description => description;
+
+        /// <inheritdoc/>
+        public override void PrepEnter()
+        {
+            base.PrepEnter();
+            _aimSystem.Initialize();
+
+            // Replace this with whatever custom indicator you need.
+            _arrowIndicator = Services.IndicatorFactory.Borrow<ArrowIndicator>();
+            DisposeOnExit(_arrowIndicator);
+        }
+
+        /// <inheritdoc/>
+        public override void PrepTick()
+        {
+            base.PrepTick();
+            var aimData = _aimSystem.Update();
+            var origin = User.transform.position;
+            var direction = (aimData.point - origin).normalized;
+            _targetPosition = Vector3.ClampMagnitude(aimData.point - origin, distance) + origin;
+            _arrowIndicator.Instance.UpdateView(origin, _targetPosition);
+
+            // By default, lock in by left-clicking. You may want a different method.
+            IsPrepFinished |= Input.GetKeyDown(KeyCode.Mouse0) && ActionTracker.CanAfford(apCost);
+        }
+
+        /// <inheritdoc/>
+        protected override IEnumerator Execute()
+        {
+            ActionTracker.Spend(apCost);
+            yield return new WaitForSeconds(0.1f);
+            var origin = User.transform.position;
+            var direction = _targetPosition - origin;
+            direction.y = 0;
+
+            // Visual effects for dashing.
+            dashEffect.InstantiateAsync(origin, Quaternion.identity);
+            yield return User.transform.TweenPosition(_targetPosition, moveDuration).SetEase(moveEasing).Await();
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
---
+++ Assets/Scripts/Application/Gameplay/Combat/Actions/FlyingMoveAction.cs
@@ -0,0 +1,106 @@
+﻿using ElRaccoone.Tweens;
+using UnityEngine.Serialization;
+
+namespace Application.Gameplay.Combat.Actions
+{
+    using System;
+    using System.Collections;
+    using Core;
+    using Core.Utility;
+    using ImGuiNET;
+    using Newtonsoft.Json;
+    using UI.Indicators;
+    using UnityEngine;
+    using UnityEngine.AI;
+
+    /// <summary>
+    /// The action where an entity moves around, in a grounded manner, on the battlefield.
+    /// </summary>
+    [Serializable]
+    [JsonObject(MemberSerialization.OptIn)]
+    public class FlyingMoveAction : BattleAction, IDebugImGui
+    {
+        [SerializeField]
+        [JsonProperty]
+        private float actionPointsPerUnit = 0.25f;
+
+        [FormerlySerializedAs("moveSpeed")]
+        [SerializeField]
+        [JsonProperty]
+        private float moveDuration = 7;
+
+        private Vector3 _targetPosition;
+        private float _distance;
+        private IPooledObject<PathIndicator> _pathIndicator;
+        private AimSystem _aimSystem = new AimSystem();
+        private ActionPointTracker _actionPointTracker;
+
+        /// <inheritdoc/>
+        public override string Name => "Move";
+
+        /// <inheritdoc/>
+        public override string Description => "Move the monster to a different location.";
+
+        private int PointCost => (int)Mathf.Ceil(_distance * actionPointsPerUnit);
+
+        /// <inheritdoc/>
+        public override void PrepEnter()
+        {
+            base.PrepEnter();
+            _pathIndicator = Services.IndicatorFactory.Borrow<PathIndicator>();
+            _aimSystem.Initialize();
+            _actionPointTracker = User.GetComponent<ActionPointTracker>();
+        }
+
+        /// <inheritdoc/>
+        public override void PrepTick()
+        {
+            base.PrepTick();
+            _targetPosition = _aimSystem.Update().point;
+
+            _distance = (_targetPosition - User.transform.position).magnitude;
+            bool canAfford = _actionPointTracker.CanAfford(PointCost);
+            IsPrepFinished |= canAfford && Input.GetKeyDown(KeyCode.Mouse0) && canAfford;
+
+            _pathIndicator.Instance.RenderPath(new[] { User.transform.position, _targetPosition });
+            _pathIndicator.Instance.IsValid = canAfford;
+        }
+
+        /// <inheritdoc/>
+        public override void PrepExit()
+        {
+            base.PrepExit();
+
+            if (!IsPrepFinished)
+            {
+                _pathIndicator.Dispose();
+            }
+        }
+
+        /// <inheritdoc/>
+        public void RenderImGui()
+        {
+            ImGui.Text($"Position: {_targetPosition}");
+            ImGui.Text($"Distance: {_distance}");
+            ImGui.Text($"Action Point Cost: {_distance:0} * {actionPointsPerUnit} = {(int)Mathf.Ceil(_distance * actionPointsPerUnit)}");
+        }
+
+        /// <inheritdoc/>
+        protected override IEnumerator Execute()
+        {
+            ActionTracker.Spend(PointCost);
+            User.transform.TweenPosition(_targetPosition, moveDuration);
+
+            float elapsedTime = 0;
+            while (elapsedTime < moveDuration)
+            {
+                elapsedTime += Time.deltaTime;
+                _pathIndicator.Instance.RenderPath(new[] { User.transform.position, _targetPosition });
+                yield return null;
+            }
+
+            _pathIndicator.Instance.ClearPath();
+            _pathIndicator.Dispose();
+        }
+    }
+}
