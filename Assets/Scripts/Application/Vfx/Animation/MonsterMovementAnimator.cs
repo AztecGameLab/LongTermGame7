2de6f9bb7a1822d0af8b999ef9e0b15d88da278d
Daniel Walls <walls.dk@gmail.com>
2023-06-19 17:31:42 -0700

    a lot of improvements to animation stuff

--- Assets/Scripts/Application/Vfx/Animation/MonsterMovementAnimator.cs
+++ Assets/Scripts/Application/Vfx/Animation/MonsterMovementAnimator.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using System.Globalization;
+using TriInspector;
 
 namespace Application.Vfx.Animation
 {
+    using System.Text;
+    using FSM;
     using UnityEngine;
 
     /// <summary>
@@ -9,8 +12,7 @@ namespace Application.Vfx.Animation
     /// </summary>
     public class MonsterMovementAnimator : MonoBehaviour
     {
-        private static readonly int Speed = Animator.StringToHash("speed");
-
+        private const float Sensitivity = 0.5f;
         private readonly VelocityTracker _velocityTracker = new VelocityTracker();
 
         [SerializeField]
@@ -19,21 +21,96 @@ namespace Application.Vfx.Animation
         [SerializeField]
         private SpriteRenderer flipSprite;
 
-        private bool _isFlipped;
+        [SerializeField]
+        private string idleStateName = "idle";
 
-        private void Awake()
+        [SerializeField]
+        private string movingStateName = "walk";
+
+        [SerializeField]
+        private float idleThreshold = 0.4f;
+
+        [SerializeField]
+        private float movingThreshold = 1f;
+
+        private StateMachine _fsm;
+        private bool _isFlipped;
+        private bool _overrideAnimations;
+
+        // Hashed versions of state names, for performance.
+        private int _idleStateHash;
+        private int _movingStateHash;
+
+        private static string GetFullName(Transform target)
         {
+            var builder = new StringBuilder(target.name);
+
+            while (target.parent != null)
+            {
+                target = target.parent;
+                builder.Insert(0, $"{target.name}/");
+            }
+
+            return builder.ToString();
+        }
+
+        private void AssertHasState(string stateName)
+        {
+            var hash = Animator.StringToHash(stateName);
+
+            if (!animator.HasState(0, hash))
+            {
+                Debug.LogError($"{GetFullName(transform)} is missing the animation state \"{stateName}\"!", gameObject);
+            }
+        }
+
+        private void Start()
+        {
+            _idleStateHash = Animator.StringToHash(idleStateName);
+            _movingStateHash = Animator.StringToHash(movingStateName);
+
+            AssertHasState(idleStateName);
+            AssertHasState(movingStateName);
+
+            _fsm = new StateMachine();
+            _fsm.AddState("idle", onEnter: _ => animator.Play(_idleStateHash, 0));
+            _fsm.AddState("walking", onEnter: _ => animator.Play(_movingStateHash, 0));
+            _fsm.AddTransition(from: "idle", to: "walking", _ => !_overrideAnimations && _velocityTracker.Speed > movingThreshold);
+            _fsm.AddTransition(from: "walking", to: "idle", _ => !_overrideAnimations && _velocityTracker.Speed < idleThreshold);
+            _fsm.SetStartState("idle");
+            _fsm.Init();
+
             _velocityTracker.Initialize(transform.position);
             _isFlipped = flipSprite.flipX;
         }
 
-        private const float Sensitivity = 0.5f;
+        [Button]
+        private void ForceIdle()
+        {
+            _overrideAnimations = true;
+            _fsm.RequestStateChange("idle");
+        }
+
+        [Button]
+        private void ForceWalking()
+        {
+            _overrideAnimations = true;
+            _fsm.RequestStateChange("walking");
+        }
+
+        [EnableIf("_overrideAnimations")]
+        [Button]
+        private void RestoreAnimations() => _overrideAnimations = false;
 
         private void Update()
         {
             _velocityTracker.Update(transform.position, Time.deltaTime);
-            animator.SetFloat(Speed, _velocityTracker.Velocity.magnitude);
+            _fsm.OnLogic();
+            UpdateFacing();
+        }
 
+        private void UpdateFacing()
+        {
             if (_velocityTracker.Velocity.x > Sensitivity)
             {
                 flipSprite.flipX = _isFlipped;
@@ -47,11 +124,5 @@ namespace Application.Vfx.Animation
                 // We not moving at all, and the sprite can just stay at whatever it was before.
             }
         }
-
-        // private void OnGUI()
-        // {
-        //     GUILayout.Label($"{_velocityTracker.Velocity.x < Sensitivity}");
-        //     GUILayout.Label($"{_velocityTracker.Velocity.x > -Sensitivity}");
-        // }
     }
 }
