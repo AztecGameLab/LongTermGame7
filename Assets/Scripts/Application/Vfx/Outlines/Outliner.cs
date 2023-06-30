2d82a51cc2844bc6ede4b29d7f0f92dad8f22249
Daniel Walls <walls.dk@gmail.com>
2023-06-30 13:25:57 -0700

    cleanup

--- Assets/Scripts/Application/Vfx/Outlines/Outliner.cs
+++ Assets/Scripts/Application/Vfx/Outlines/Outliner.cs
@@ -1,40 +1,8 @@
-﻿using Application.Core;
-using System;
-
-namespace Application.Vfx
+﻿namespace Application.Vfx
 {
     using System.Collections.Generic;
     using ElRaccoone.Tweens;
     using UnityEngine;
-
-    public class OutlineInstance : IDisposable
-    {
-        private readonly Material _material;
-        private readonly GameObject _source;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="OutlineInstance"/> class.
-        /// </summary>
-        /// <param name="material">The material this outline uses.</param>
-        /// <param name="source">The object this outline is bound to.</param>
-        public OutlineInstance(Material material, GameObject source)
-        {
-            _material = material;
-            _source = source;
-        }
-
-        public Color Color
-        {
-            get => _material.color;
-            set => _material.color = value;
-        }
-
-        /// <inheritdoc/>
-        public void Dispose()
-        {
-            Services.Outliner.RemoveOutline(_source);
-        }
-    }
 
     /// <summary>
     /// Creates outlines for GameObjects.
@@ -54,11 +22,26 @@ namespace Application.Vfx
             _outlineMaterial = outlineMaterial;
         }
 
-        public void AddOutline(GameObject target, float duration = 0.5f)
+        /// <summary>
+        /// Add an outline to an object in the world.
+        /// This only works for objects with one <see cref="SpriteRenderer"/> in their children.
+        /// </summary>
+        /// <param name="target">The object to add an outline to.</param>
+        /// <param name="duration">The animation speed for the outline appearing.</param>
+        /// <returns>An instance of the created outline - can be used for easy control.</returns>
+        public OutlineInstance AddOutline(GameObject target, float duration = 0.5f)
         {
-            AddOutline(target, Color.white);
+            return AddOutline(target, Color.white);
         }
 
+        /// <summary>
+        /// Add an outline to an object in the world.
+        /// This only works for objects with one <see cref="SpriteRenderer"/> in their children.
+        /// </summary>
+        /// <param name="target">The object to add an outline to.</param>
+        /// <param name="color">The color the outline should initially be.</param>
+        /// <param name="duration">The animation speed for the outline appearing.</param>
+        /// <returns>An instance of the created outline - can be used for easy control.</returns>
         public OutlineInstance AddOutline(GameObject target, Color color, float duration = 0.5f)
         {
             if (!_targetsToOutlines.ContainsKey(target))
@@ -66,6 +49,7 @@ namespace Application.Vfx
                 var targetRenderer = target.GetComponentInChildren<SpriteRenderer>();
                 var state = new SavedRenderState { Material = targetRenderer.material };
                 float from = targetRenderer.material.HasProperty("_Color") ? targetRenderer.material.color.a : 0;
+
                 targetRenderer.material = _outlineMaterial;
                 targetRenderer.material.color = color;
                 targetRenderer.TweenCancelAll();
@@ -83,12 +67,18 @@ namespace Application.Vfx
             return null;
         }
 
+        /// <summary>
+        /// Removes an outline from an object in the world, if it has one.
+        /// </summary>
+        /// <param name="target">The object that's outline should be removed.</param>
+        /// <param name="duration">The duration of the fading out animation.</param>
         public void RemoveOutline(GameObject target, float duration = 0.5f)
         {
             if (_targetsToOutlines.TryGetValue(target, out SavedRenderState state))
             {
                 var renderer = target.GetComponentInChildren<SpriteRenderer>();
                 var from = renderer.material.color.a;
+
                 renderer.TweenCancelAll();
                 renderer.TweenValueFloat(0, duration, value =>
                 {
@@ -101,6 +91,9 @@ namespace Application.Vfx
             }
         }
 
+        /// <summary>
+        /// Used for restoring a renderer after removing its outline.
+        /// </summary>
         private struct SavedRenderState
         {
             public Material Material;
