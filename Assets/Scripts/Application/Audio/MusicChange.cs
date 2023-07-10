8205b82d147ab53038e549883d53fe181358e4a2
Daniel Walls <walls.dk@gmail.com>
2023-07-09 23:37:22 -0700

    sfx for levels

---
+++ Assets/Scripts/Application/Audio/MusicChange.cs
@@ -0,0 +1,33 @@
+﻿namespace Application.Audio
+{
+    using Core;
+    using FMODUnity;
+    using UnityEngine;
+
+    public class MusicChange : MonoBehaviour
+    {
+        public EventReference music;
+
+        private static EventReference _activeMusic;
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        private static void ResetStatics()
+        {
+            _activeMusic = default;
+        }
+
+        private void Start()
+        {
+            if (_activeMusic.Guid != music.Guid)
+            {
+                foreach (MusicPlayer.ActiveMusic activeMusic in Services.MusicPlayer.MusicList)
+                {
+                    activeMusic.Dispose();
+                }
+
+                Services.MusicPlayer.AddMusic(music);
+                _activeMusic = music;
+            }
+        }
+    }
+}
