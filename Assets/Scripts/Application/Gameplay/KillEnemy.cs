99f48ec3b7acdbde9d9eefcbe96a6405b7d92a41
JonnyTheKing-3 <jonathangrcrdrgz@gmail.com>
2023-07-19 19:12:19 -0700

    KillEnemy Script
    Script to kill enemy once it loses

--- Assets/Scripts/Application/Gameplay/KillEnemy.cs
+++ Assets/Scripts/Application/Gameplay/KillEnemy.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Application.Gameplay;
+using UniRx;
 
-public class NewBehaviourScript : MonoBehaviour
+public class KillEnemy : MonoBehaviour
 {
-    // if (LivingEntity == false) {
-    //     //kill 
-    // }
+    [SerializeField] private LivingEntity entity;
+
+    public void Start() {
+    entity.OnDeath.Subscribe(Lloyd => HandleEntityDeath());
+    }
+
+    private void HandleEntityDeath(){
+        Destroy(gameObject);
+    }
+
 }
