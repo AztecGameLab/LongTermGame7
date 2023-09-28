62fa7fd432008b26f6deabcf1e44c5f0f083c7f7
Daniel Walls <walls.dk@gmail.com>
2023-09-28 14:32:36 -0700

    fixed credits scnee

---
+++ Assets/Scripts/Application/Vfx/CanvasCameraFinder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace Application.Vfx
+{
+    public class CanvasCameraFinder : MonoBehaviour
+    {
+        private void Start()
+        {
+            if (TryGetComponent(out Canvas canvas))
+                canvas.worldCamera = Camera.main;
+        }
+    }
+}
---
+++ Assets/Scripts/Application/Vfx/CreditsScroller.cs
@@ -0,0 +1,60 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+using System;
+using UnityEngine;
+
+namespace Application.Vfx
+{
+
+    public class CreditsScroller : MonoBehaviour
+    {
+
+#if UNITY_EDITOR
+        [CustomEditor(typeof(CreditsScroller))]
+        private class EditorLogic : Editor
+        {
+            private void OnSceneGUI()
+            {
+                var t = target as CreditsScroller;
+                t.startY = Handles.PositionHandle(t.transform.position + t.startY, t.transform.rotation) - t.transform.position;
+                t.endY = Handles.PositionHandle(t.transform.position + t.endY, t.transform.rotation) - t.transform.position;
+                Handles.Label(t.transform.position, $"{t._elapsedTime}");
+            }
+        }
+#endif
+
+        [SerializeField]
+        private float duration = 120;
+
+        [SerializeField]
+        private Vector3 startY;
+
+        [SerializeField]
+        private Vector3 endY;
+
+        private float _elapsedTime;
+
+        private void Start()
+        {
+            Vector3 startPosition = transform.position;
+            startY += startPosition;
+            endY += startPosition;
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawSphere(startY, 0.5f);
+            Gizmos.DrawSphere(endY, 0.5f);
+            Gizmos.DrawLine(transform.position + startY, transform.position + endY);
+        }
+
+        private void Update()
+        {
+            _elapsedTime += Time.deltaTime;
+            float t = Mathf.Clamp01(_elapsedTime / duration);
+            transform.position = Vector3.Lerp(startY, endY, t);
+        }
+    }
+}
